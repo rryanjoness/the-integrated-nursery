ad1b4b13342d2a6b594b869f744496d252750a18
rryanjoness <161358498+rryanjoness@users.noreply.github.com>
2024-02-26 18:22:16 -0500

    Update Plant.java

--- TheIntegratedNursery/src/Plant.java
+++ TheIntegratedNursery/src/Plant.java
@@ -12,19 +12,7 @@ public class Plant {
     String commonName;
     LocalDate dateIntroduced;
     public static HashMap<Integer, Zone> zones = new HashMap<Integer, Zone>();
-    static {
-        zones.put(1, new Zone(-9999, -50));
-        zones.put(2, new Zone(-50, -40));
-        zones.put(3, new Zone(-40, -30));
-        zones.put(4, new Zone(-30, -20));
-        zones.put(5, new Zone(-20, -10));
-        zones.put(6, new Zone(-10, 0));
-        zones.put(7, new Zone(0, 10));
-        zones.put(8, new Zone(10, 20));
-        zones.put(9, new Zone(20, 30));
-        zones.put(10, new Zone(30, 40));
-        zones.put(11, new Zone(40, 50));
-    }
+    
 
 
     enum plantGroup {
