5902aaf725e2abb0e163ef405fdf5a83ebc96262
rryanjoness <161358498+rryanjoness@users.noreply.github.com>
2024-02-28 13:00:58 -0500

    Update Zone.java

--- TheIntegratedNursery/src/Zone.java
+++ TheIntegratedNursery/src/Zone.java
@@ -2,10 +2,29 @@ import java.util.HashMap;
 
 public class Zone {
 
-    public static HashMap<Integer, Zone> Zones = new HashMap<Integer, Zone>();
-    static 
-    {
-       // Zones.put() 
+    @SuppressWarnings("unused")
+    private int minTemp;
+    @SuppressWarnings("unused")
+    private int maxTemp;
+
+    private static HashMap<Integer, Zone> zones;
+    static {
+        zones.put(1, new Zone(-2147483648, -50));
+        zones.put(2, new Zone(-50, -40));
+        zones.put(3, new Zone(-40, -30));
+        zones.put(4, new Zone(-30, -20));
+        zones.put(5, new Zone(-20, -10));
+        zones.put(6, new Zone(-10, 0));
+        zones.put(7, new Zone(0, 10));
+        zones.put(8, new Zone(10, 20));
+        zones.put(9, new Zone(20, 30));
+        zones.put(10, new Zone(30, 40));
+        zones.put(11, new Zone(40, 50));
     }
-    
+
+    private Zone(int minTemp, int maxTemp){
+    this.minTemp = minTemp;
+    this.maxTemp = maxTemp;
+    }
+
 }
